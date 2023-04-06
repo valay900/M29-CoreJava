583e27607c6537218c9bc454adc9e97f9e9c148d
valay900 <valaychop@gmail.com>
2023-04-06 18:01:22 +0530

    commit

---
+++ dayone/src/org/tnsif/introduction/FirstProgram.java
@@ -0,0 +1,10 @@
+package org.tnsif.introduction;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Hello world");
+	}
+
+}
