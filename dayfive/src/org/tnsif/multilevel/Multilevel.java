7662c49e0b9097de182a6c148ef1aa7160be2364
valay900 <valaychop@gmail.com>
2023-04-25 16:57:15 +0530

    second commit

---
+++ dayfive/src/org/tnsif/multilevel/Multilevel.java
@@ -0,0 +1,15 @@
+package org.tnsif.multilevel;
+
+public class Multilevel {
+
+	public static void main(String[] args) {
+		City c=new City();
+		c.setCityName("Boisar");
+		c.setArea("Bhimnagar");
+		c.setStateName("Maharashtra");
+		c.setCountryName("India");
+		c.setCountryCapital("Delhi");
+		System.out.println(c);
+	}
+
+}
---
+++ dayfive/src/org/tnsif/superkeyword/SuperExecutor.java
@@ -0,0 +1,12 @@
+package org.tnsif.superkeyword;
+
+public class SuperExecutor {
+
+	public static void main(String[] args) {
+		Tiger t=new Tiger();
+		t.display();
+
+	}
+
+}
+
